b81be34a4077c0247d52e6758465313c17bdab46
Jonas <jonasper19@gmail.com>
2025-04-18 17:27:31 -0300

    small changes

--- ex05/src/main.cpp
+++ ex05/src/main.cpp
@@ -3,12 +3,21 @@
 
 int main(void)
 {
-	Harl	h;
+	Harl		h;
+	std::string	input;
 
-	h.complain("DEBUG");
-	h.complain("INFO");
-	h.complain("ERROR");
-	h.complain("WARNING");
-	h.complain("Wfdgdfg");
+	while (1)
+	{
+		std::cout << ORANGE << std::string(40, '-') << "\n";
+		std::cout << "\tHarl\n" << std::flush;
+		std::cout << std::string(40, '-') << "\n" RESET;
+
+		std::cout << BRIGHT_GREEN "Hou to use: [WARNING] [ERROR] [DEBUG] [INFO]\n" RESET;
+		std::cout << ">$ ";
+		if (!std::getline(std::cin, input))
+			return (0);
+		if (!input.empty())
+			h.complain(input);
+	}
 	return (0);
 }
